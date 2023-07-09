bcba5875be6f2ac302861a0d0af0c9a07d1d7358
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-09 17:51:20 -0400

    update lib

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+# Calling the happy_new_year function
+happy_new_year()
+
+
+ 
+
+def square_integers(numbers):
+    squared_list = []
+    for num in numbers:
+        squared_list.append(num ** 2)
+    return squared_list
+
+# Calling the square_integers function
+result = square_integers([1, 2, 3, 4, 5])
+print(result)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")   
+        else:
+            print(num)
+
+fizzbuzz()
